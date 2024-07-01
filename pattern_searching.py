0c1344289c8a3c5728f84cab5ec125f7081eecd0
YaminiPriyaK <138279273+YaminiPriyaK@users.noreply.github.com>
2024-07-01 10:13:13 +0530

    Create pattern_searching.py

---
+++ pattern_searching.py
@@ -0,0 +1,13 @@
+# pattern searching
+
+S='JBABCABCABHJHGJABCABCABCABCABABCABCABCABCAC'
+p='ABCAB'
+for i in range(len(S)):
+    if p in S:
+        e=(S.index(p))
+        print(e)
+        if(S[e]==p[0]):
+                S=list(S)
+                S[e]=" "
+                e=e+1
+        S=''.join(S)
